d149a0428dfe12b688d0d27c5947f20b64b2e04b
Pro696969 <krishnakumaarmc@gmail.com>
2024-07-06 22:22:02 +0530

    Fixed cd to handle most cases and added username@PSH
    Co-authored-by: Aditya/WIZ <pro.aditya.r@gmail.com>

--- src/main.c
+++ src/main.c
@@ -2,41 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <time.h>
 #include <unistd.h>
 
 int PSH_READ();
 char **PSH_TOKENIZER(char *);
-int PSH_EXEC_EXTERNAL(char **);   
+int PSH_EXEC_EXTERNAL(char **);
 int PSH_EXIT(char **);
 int PSH_CD(char **);
 int PSH_ECHO(char **);
 int PSH_PWD(char **);
 int PSH_HISTORY(char **);
 
-
 // if u want to continue execution return 1 in built_in funcs
+
+static char PREV_DIR[1024];
 
 char *builtin_str[] = {"exit", "cd", "echo", "pwd", "history"};
 
 int (*builtin_func[])(char **) = {
-    &PSH_EXIT,
-    &PSH_CD,
-    &PSH_ECHO, 
-    &PSH_PWD, 
-    &PSH_HISTORY
-    }; // function pointer that takes in array of strings and return an int)
+    &PSH_EXIT, &PSH_CD, &PSH_ECHO, &PSH_PWD,
+    &PSH_HISTORY}; // function pointer that takes in array of strings and return
+                   // an int)
 
 int size_builtin_str = sizeof(builtin_str) / sizeof(builtin_str[0]); // number of built_in args
 
-int main() {
+int main(int argc, char **argv, char **envp) {
   printf("Welcome to psh!\n");
+
+  // printf("%s@", getenv("USER"));
+  // for (int i=0; envp[i]!=NULL; i++) {  // accessing all env variables
+  //       printf("%d: %s\n", i, envp[i]);
+  //   }
+
   return PSH_READ();
 }
 
 int PSH_EXIT(char **token_arr) {
   if (!token_arr[1]) {
-    printf("bye bye PSH :D\n"); // handling empty args and freeing token array before leaving
+    printf("bye bye PSH :D\n"); // handling empty args and freeing token array
+                                // before leaving
     free(token_arr);
     return 0;
     // exit(0);
@@ -48,46 +52,77 @@ int PSH_EXIT(char **token_arr) {
   // exit(exit_code);
 }
 
-int PSH_CD(char **token_arr) { //kk
+int PSH_CD(char **token_arr) {
+  char current_dir[1024];
 
-  if (token_arr[1] == NULL) {
-    fprintf(stderr, "PSH: expected argument to \"cd\"\n");
+  if (getcwd(current_dir, sizeof(current_dir)) == NULL) {
+    perror("PSH: getcwd() error");
     return 1;
   }
 
-  if (chdir(token_arr[1]) == -1) {
-    fprintf(stderr, "PSH: no such file/directory exists\n");
+  char *home = malloc(1024 * sizeof(char *));
+  if (token_arr[1] == NULL || (strcmp(token_arr[1], "~") == 0)) {
+    home = getenv("HOME");
+    if (chdir(home) == -1) {
+      perror("PSH: cd");
+      free(home);
+      return 1;
+    }
   }
 
+  else if (strcmp(token_arr[1], "-") == 0) {
+    if (PREV_DIR[0] == '\0') {
+      fprintf(stderr, "PSH: No previous directory\n");
+      return 1;
+    }
+    // printf("%s\n", PREV_DIR); debugging
+    if (chdir(PREV_DIR) == -1) {
+      perror("PSH: chdir() error");
+      return 1;
+    }
+  } 
+
+  else {
+    if (chdir(token_arr[1]) == -1) {
+      fprintf(stderr, "PSH: no such file/directory exists\n");
+      return 1;
+    }
+  }
+  // Update PREV_DIR after successful directory change
+  strcpy(PREV_DIR, current_dir);
+
+  // free(home);
   return 1;
 }
 
-int PSH_ECHO(char **token_arr) { //   
-      // Sid
-//   int i = 1;
-//   printf("%s",token_arr[0]); // debugging
-//   // while (token_arr[i] != "\n") {
-//   // printf("%s",token_arr[i]);
-//   // }
+int PSH_ECHO(char **token_arr) { //
+                                 // Sid
+                                 //   int i = 1;
+                                 //   printf("%s",token_arr[0]); // debugging
+                                 //   // while (token_arr[i] != "\n") {
+                                 //   // printf("%s",token_arr[i]);
+                                 //   // }
   return 1;
 }
 
 int PSH_PWD(char **token_arr) {
-  //your code goes here SUMITHRA
+  // your code goes here SUMITHRA
   return 1;
 }
 
 int PSH_HISTORY(char **token_arr) {
-  //your code goes here Alayna
+  // your code goes here Alayna
   return 1;
-} 
+}
+
 int PSH_READ() {
   size_t n = 0;
-  int run = 1     ;
-  char *inputline = NULL;        // NULL is required to avoind conflicts with getline function
+  int run = 1;
+  char *inputline =
+      NULL;        // NULL is required to avoind conflicts with getline function
   while (run == 1) // if not done stack smashing occurs
   {
-    printf("PSH $ ");
+    printf("%s@PSH $ ", getenv("USER"));
     if (getline(&inputline, &n, stdin) == -1) {
       perror("getline");
       free(inputline);
@@ -98,19 +133,17 @@ int PSH_READ() {
     // line is used to remove that \n and changing it blank space
 
     char **token_arr = PSH_TOKENIZER(inputline);
-    if (token_arr[0]!= NULL)      //fixed \n giving seg fault
+    if (token_arr[0] != NULL) // fixed \n giving seg fault
     {
       int isinbuilt = 0;
       for (int i = 0; i < size_builtin_str; i++) {
-        if (strcmp(token_arr[0], builtin_str[i])==0)
-        {     
-          if(!strcmp(token_arr[0],"exit"))
-          {
+        if (strcmp(token_arr[0], builtin_str[i]) == 0) {
+          if (!strcmp(token_arr[0], "exit")) {
             run = (*builtin_func[i])(token_arr);
             free(inputline);
             exit(run);
           }
-          run = (*builtin_func[i])(token_arr);  
+          run = (*builtin_func[i])(token_arr);
           isinbuilt = 1;
           break;
         } // frees token_arr
@@ -133,7 +166,7 @@ char **PSH_TOKENIZER(char *line) {
     exit(EXIT_FAILURE);
   }
   token = strtok(line, " ");
-  while (token != NULL) // making an array of token_arr by seperating the line
+  while (token != NULL) // making an array of token_arr by separating the line
   {                     // one by one as delim as " "
     token_arr[position] = token;
     position++;
@@ -157,6 +190,7 @@ char **PSH_TOKENIZER(char *line) {
   // }
   return token_arr;
 }
+
 int PSH_EXEC_EXTERNAL(char **token_arr) {
   pid_t pid, wpid;
   int status;
@@ -164,7 +198,8 @@ int PSH_EXEC_EXTERNAL(char **token_arr) {
   // printf("here"); //debugging
   if (pid == 0) {
     // Child process
-    if (execvp(token_arr[0], token_arr) == -1) // executes the binary file with args
+    if (execvp(token_arr[0], token_arr) ==
+        -1) // executes the binary file with args
     {
       perror("psh failed");
     }
