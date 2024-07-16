aa65bf7f886adccf61381ca1d9fb8f21d0d54054
Sumithra Suresh <2sumithra.suresh@gmail.com>
2024-07-17 00:21:53 +0530

    Updated Tokenizer to recognize quotes

--- src/execute.c
+++ src/execute.c
@@ -2,9 +2,12 @@
 
 char **PSH_TOKENIZER(char *line)
 {
-  size_t bufsize = 64, position = 0;
+  size_t bufsize = 64, position = 0, i;
   char **token_arr = malloc(bufsize * sizeof(char *));
   char *token;
+  int qstring = 0, has_quote = 0;
+  const char *delimiters = " \t\n";
+  char quote;
   if (!token_arr)
   {
     fprintf(stderr, "psh: allocation error\n");
@@ -12,9 +15,58 @@ char **PSH_TOKENIZER(char *line)
   }
   token = strtok(line, " ");
   while (token != NULL) // making an array of token_arr by separating the line
-  {                     // one by one as delim as " "
-    token_arr[position] = token;
-    position++;
+  {                // one by one as delim as " "
+    size_t len = strlen(token);
+    for(i = 0; i < strlen(token); i++)
+    {       
+        //printf("Token %c\n", token[i]); // DEBUGGINGi
+        if ((has_quote == 0) && (token[i] == '"' || token[i] == '\''))
+        {
+          //printf("Accepted token: %c\n", token[i]);
+          quote = token[i];
+          has_quote = 1;
+          break;
+        }
+        else if (token[i] == quote)
+        {
+          //printf("Accepted token: %c\n", token[i]);
+          has_quote = 1;
+          break;
+        }
+        else
+        {
+          has_quote = 0;
+        }
+    }
+    // Remove quote
+    if (has_quote == 1)
+    {
+      for (size_t j = i + 1; j < strlen(token); j++)
+      {
+        token[j - 1] = token[j];
+      }
+      token[strlen(token) - 1] = '\0';
+      //printf("After quotes: %s\n", token); // DEBUGGING
+      len = strlen(token);
+    }
+    if (qstring == 0)
+    {
+      token_arr[position] = malloc((len + 1) * sizeof(char));
+      strcpy(token_arr[position], token);
+      position++;
+    }
+    else
+    {
+      size_t prev_len = strlen(token_arr[position - 1]);
+      token_arr[position - 1] = realloc(token_arr[position - 1], (prev_len + len + 2) * sizeof(char));
+      if (token[len - 1] == quote)
+      {
+        token[len - 1] = '\0';
+      }
+      strcat(token_arr[position - 1], " ");
+      strcat(token_arr[position - 1], token);
+      //printf("Added tokens: %s\n", token_arr[position - 1]);
+    }
     if (position >= bufsize)
     {
       bufsize += 64;
@@ -25,18 +77,22 @@ char **PSH_TOKENIZER(char *line)
         exit(EXIT_FAILURE);
       }
     }
-    token = strtok(NULL, " ");
+    if (has_quote == 1)
+    {
+      qstring = !(qstring);
+    }
+    token = strtok(NULL, delimiters);
   }
-  token_arr[position] = NULL; // terminating
-  // int i=0;
-
-  // while(token_arr[i]!=NULL)            //debugging token_arr array
+  token_arr[position] = NULL;
+  // int k = 0;
+  // while(token_arr[k]!=NULL)            //debugging token_arr array
   // {
-  //     printf("%s\n",token_arr[i]);
-  //     i++;
+  //     printf("%s\n",token_arr[k]);
+  //     k++;
   // }
   return token_arr;
 }
+
 
 int PSH_EXEC_EXTERNAL(char **token_arr)
 {
